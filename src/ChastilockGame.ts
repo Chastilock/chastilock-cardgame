813a4297613fac453188fb5c327b3c84f50247ed
ed-5on2 <>
2021-06-04 13:45:35 -0400

    Modified ChastilockGame.drawCard() to use Lock.drawCard()

--- src/ChastilockGame.ts
+++ src/ChastilockGame.ts
@@ -4,8 +4,8 @@ import Lock from 'model/Lock'
 import CardApplierManager from 'cards/CardApplierManager'
 
 class ChastilockGame {
-  public config: Config
-  public lock: Lock
+  public config: Config // the configuration of maximum cards, used to limit the lock
+  public lock: Lock // the lock also has a config which contains info about the min/max cards specified when lock created
   public manager: CardApplierManager = new CardApplierManager()
 
   constructor (config: Config, lock: Lock) {
@@ -14,7 +14,7 @@ class ChastilockGame {
   }
 
   drawCard (): CardType {
-    const drawn = this.lock.getCards().drawRandomType()
+    const drawn = this.lock.drawCard()
 
     // Apply the effects
     this.manager.apply(this.lock, drawn)
