156c5545ca0eb51947bc3d37b20cba51cbf6fb8c
ed-5on2 <>
2021-06-05 12:38:42 -0400

    Modified FreezeApplier to add Freeze to lock

--- src/cards/appliers/FreezeCardApplier.ts
+++ src/cards/appliers/FreezeCardApplier.ts
@@ -1,6 +1,7 @@
 import CardType from 'model/CardType'
 import Lock from 'model/Lock'
 import CardApplier from 'cards/CardApplier'
+import Freeze, { FreezeType } from 'model/Freeze'
 
 class FreezeCardApplier implements CardApplier {
   canHandle (type: CardType): boolean {
@@ -14,6 +15,13 @@ class FreezeCardApplier implements CardApplier {
    */
   apply (lock: Lock): void {
     const multiplier = 2 + Math.random() * 2
+
+    let cardFreeze: Freeze = {
+      source: FreezeType.CARD,
+      startTime: lock.elapsedMinutes,
+      endTime: lock.elapsedMinutes + multiplier * lock.config.intervalMinutes
+    }
+    lock.freeze = cardFreeze
 
     lock.nextDraw = lock.config.intervalMinutes * multiplier
     lock.getCards().setCardsOfType(CardType.FREEZE, lock.getCards().getFreeze() - 1)
