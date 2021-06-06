47dc3116ca8cae45baf93343f1050e98173fba9d
ed-5on2 <>
2021-06-05 22:15:48 -0400

    Added MAX_CARDS configuration file to specify max amount of each card

---
+++ src/MaxCardConfig.ts
@@ -0,0 +1,19 @@
+import Config from './model/Config'
+
+const MAX_CARDS: Config = {
+  max: {
+    GREEN: 100, /* Same as CK */
+    RED: 599, /* Same as CK */
+    STICKY: 100, /* Same as CK */
+    'YELLOW+1': 299, /* Same as CK */
+    'YELLOW+2': 299, /* Same as CK */
+    'YELLOW+3': 299, /* Same as CK */
+    'YELLOW-1': 299, /* Same as CK */
+    'YELLOW-2': 299, /* Same as CK */
+    FREEZE: 100, /* Same as CK */
+    DOUBLE: 100, /* Same as CK */
+    RESET: 100 /* Same as CK */
+  }
+}
+
+export default MAX_CARDS
